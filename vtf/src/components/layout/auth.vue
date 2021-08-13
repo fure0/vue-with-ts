dbeb025c1964bfd90939c3e2af7be71aa100cab6
KimTaeyoun <kimty1121@gmail.com>
2021-08-13 23:55:13 +0900

    make components

---
+++ vtf/src/components/layout/auth.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-app>
+        <v-main>
+            <v-container class="justify-center align-center fill-height">
+                <v-textarea outlined></v-textarea>
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class LayoutAuth extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+</style>
